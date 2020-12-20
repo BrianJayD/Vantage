adcdbdeb8045d4b7e6ad25d10b9aacf7ced056b0
BrianJayD <14907362+BrianJayD@users.noreply.github.com>
2020-12-20 01:39:36 -0500

    Initial Commit

---
+++ Vantage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Vantage
+//
+//  Created by Brian Domingo on 2020-12-20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
